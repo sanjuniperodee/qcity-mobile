2744b422f50cbfde048e2e814a1245ba4c68872f
sanjuniperodee <87779571856b@gmail.com>
2025-11-30 23:31:25 +0500

    fix

--- navigation/index.js
+++ navigation/index.js
@@ -24,70 +24,75 @@ export default function Navigation() {
       prefixes: ['https://www.qcity.kz', 'qcity://'],
       config: {
         screens: {
-          HomeTab: {
+          root: {
             path: '',
             screens: {
-              Home: {
+              HomeTab: {
                 path: '',
-                exact: true,
+                screens: {
+                  Home: {
+                    path: '',
+                    exact: true,
+                  },
+                  ViewPost: 'post/:id',
+                  ViewUser: 'user/:username',
+                  ResultsSearchScreen: 'search/results',
+                  SearchScreen: 'search',
+                  GetPostsByCity: 'city/:city',
+                  GetPostsByCategory: 'category/:id',
+                  QorgauAi: 'ai',
+                  Support: 'support',
+                },
               },
-              ViewPost: 'post/:id',
-              ViewUser: 'user/:username',
-              ResultsSearchScreen: 'search/results',
-              SearchScreen: 'search',
-              GetPostsByCity: 'city/:city',
-              GetPostsByCategory: 'category/:id',
-              QorgauAi: 'ai',
-              Support: 'support',
-            },
-          },
-          // Избранные
-          FeedTab: {
-            path: 'favourites',
-            screens: {
-              Home: '',
-            },
-          },
-          // Создание поста
-          CreateTab: {
-            path: 'create',
-            screens: {
-              CreatePostCategory: '',
-              CreatePost: 'details',
-              PostTariffs: 'tariffs',
-              Pay: 'pay',
-              PostCreated: 'success',
-            },
-          },
-          MessagesTab: {
-            path: 'messages',
-            screens: {
-              Messages: '',
-              MessagesDm: ':username',
-            },
-          },
-          ProfileTab: {
-            path: 'profile',
-            screens: {
-              Profile: '',
-              Policy: 'policy',
-              Terms: 'terms',
-              about: 'about',
-              tariffs: 'tariffs',
-              active: 'active',
-              edit: 'edit/:postId?',
-              approve: 'approve',
-              Favourite: 'favourites',
-              notactive: 'not-active',
-              notpayed: 'not-paid',
-              deleted: 'deleted',
-              admin: 'admin',
-              adminApproved: 'admin/approved',
-              adminRejected: 'admin/rejected',
-              adminUsers: 'admin/users',
-              ProfileSettings: 'settings',
-              PostTariffs: 'tariffs/select',
-              Pay: 'pay',
+              // Избранные
+              FeedTab: {
+                path: 'favourites',
+                screens: {
+                  Home: '',
+                },
+              },
+              // Создание поста
+              CreateTab: {
+                path: 'create',
+                screens: {
+                  CreatePostCategory: '',
+                  CreatePost: 'details',
+                  PostTariffs: 'tariffs',
+                  Pay: 'pay',
+                  PostCreated: 'success',
+                },
+              },
+              MessagesTab: {
+                path: 'messages',
+                screens: {
+                  Messages: '',
+                  MessagesDm: ':username',
+                },
+              },
+              ProfileTab: {
+                path: 'profile',
+                screens: {
+                  Profile: '',
+                  Policy: 'policy',
+                  Terms: 'terms',
+                  about: 'about',
+                  tariffs: 'tariffs',
+                  active: 'active',
+                  edit: 'edit/:postId?',
+                  approve: 'approve',
+                  Favourite: 'favourites',
+                  notactive: 'not-active',
+                  notpayed: 'not-paid',
+                  deleted: 'deleted',
+                  admin: 'admin',
+                  adminApproved: 'admin/approved',
+                  adminRejected: 'admin/rejected',
+                  adminUsers: 'admin/users',
+                  ProfileSettings: 'settings',
+                  PostTariffs: 'tariffs/select',
+                  Pay: 'pay',
+                },
+              },
             },
           },
           Auth: {
